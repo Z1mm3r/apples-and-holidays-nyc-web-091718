3fab28a8d0b554fd51d00a0305ac870ddb41e7e0
Casey Zimmer <czimmer@ramapo.edu>
2018-09-16 22:24:11 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,9 +65,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   
-  puts holiday_hash
-  
   holiday_hash.each do |season, holiday|
+    puts holiday
     puts "#{season.to_s.capitalize}:"
     output_string = ""
    
